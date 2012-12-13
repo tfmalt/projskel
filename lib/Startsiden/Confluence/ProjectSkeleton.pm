a7eab9284c5efe0b01804f8fb079cf2b26dfd2e4
Thomas Malt <thomas@malt.no>
2012-12-13 17:46:54 +0100

    first version where generating all pages should work

--- lib/Startsiden/Confluence/ProjectSkeleton.pm
+++ lib/Startsiden/Confluence/ProjectSkeleton.pm
@@ -10,7 +10,7 @@ use Confluence;
 
 has 'config' => (
 	isa     => 'HashRef',
-	is      => 'ro',
+	is      => 'rw',
 	lazy    => 1,
 	builder => '_load_config',
 );
@@ -45,6 +45,8 @@ has 'title' => (
     lazy => 1,
     builder => '_fetch_title',
 );
+
+has 'homepage_id' => ( isa => 'Int', is  => 'rw' );
 
 sub _fetch_space {
     my $self = shift;
@@ -91,17 +93,89 @@ sub _build_wiki {
 sub create_project_skeleton {
     my $self = shift;
 
-    $self->update_homepage();
+    my $c    = $self->config;
+    my $w    = $self->wiki;
+    my $ts   = $c->{template_space};
+    my $templates  = $c->{templates};
+ 
+    if (ref($templates) ne 'HASH') {
+        croak "templates list from config is not an hash. syntax error.";
+    }
+
+    if (!defined $templates->{homepage}) {
+        croak "homepage template not defined. syntax error.";
+    }
+    
+    my $tmpl = undef;
+    # Create and set the root page of the project    
+    $self->create_project_homepage();
+
+    foreach (my $key = keys %{$templates}) {
+        next if ($key eq 'homepage');
+        $self->create_page_from_template($key);
+    }
+ 
+    return 1;
 }
 
-sub update_homepage {
+sub create_project_homepage {
     my $self = shift;
 
+    my $w  = $self->wiki;
+    my $c  = $self->config;
+    my $ts = $c->{template_space};
+
+    # Create and set the root page of the project    
+    my $root = $w->getPage($self->space, $self->title);
+    my $tmpl = $w->getPage($ts, $c->{templates}->{homepage}->{name});
+
+    $root->{content} = $tmpl->{content};
+    $c->{templates}->{homepage}->{id}    = $root->{id};
+    $c->{templates}->{homepage}->{title} = $root->{title}; 
+    $w->storePage($root);
+
+    return 1;
+}
+
+sub get_parent_id {
+    my ($self, $key) = @_;
+
+    my $c      = $self->config;
+    my $parent = $c->{templates}->{$key}->{parent};
+
+    return $c->{templates}->{$parent}->{id}; 
+}
+
+sub set_page_id {
+    my ($self, $key, $id) = @_;
+
+    my $c = $self->config;
+    # TODO add assertions haha.
+    $c->{templates}->{$key}->{id} = $id;
+
+    return 1;
+}
+
+sub create_page_from_template {
+    my $self = shift;
+    my $key  = shift;
+ 
     my $w    = $self->wiki;
     my $c    = $self->config;
+    my $ts   = $c->{template_space};
+    my $name = $c->{templates}->{$key}->{name};
+    my $tmpl = $w->getPage($ts, $name); 
 
-    my $home      = $w->getPage($self->space, $self->title);
-    my $home_tmpl = $w->getPage($c->{template_space}, $c->{templates}->{homepage}); 
+    my $page = {
+        space    => $self->space,
+        title    => $c->{templates}->{$key}->{title} . ' - ' 
+                  . $c->{templates}->{homepage}->{title},
+        content  => $tmpl->{content},
+        parentId => $self->get_parent_id($key),
+    };
+
+    $page = $w->storePage($page);
+    $self->set_page_key($key, $page->{id});
     
     return 1;
 }
